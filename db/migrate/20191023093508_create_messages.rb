cd65fa88ac42ca654d7319bb34adab84d8a5d22f
lewis nyoike <nyoikelewis@gmail.com>
2019-10-23 14:04:27 +0300

    Changed from integer to string as a work around for now

--- db/migrate/20191023093508_create_messages.rb
+++ db/migrate/20191023093508_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.text :body
-      t.integer :to
+      t.string :to
 
       t.timestamps
     end
